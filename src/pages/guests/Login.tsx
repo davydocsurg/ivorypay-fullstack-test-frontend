0f436000f2a2ea9e370fab0879bc257f0a4f67be
davydocsurg <davydocsurg@gmail.com>
2023-08-25 09:32:26 +0100

    update registration and login pages

--- src/pages/guests/Login.tsx
+++ src/pages/guests/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from "react";
-import { UserOutlined, LockFilled } from "@ant-design/icons";
+import { UserOutlined, LockFilled, WalletOutlined } from "@ant-design/icons";
 import { Button, Input } from "../../components";
 import { Link } from "react-router-dom";
 import { Form } from "@unform/web";
@@ -10,6 +10,7 @@ import { Toast } from "../../utils";
 import { LoginCredentials } from "../../types";
 import { httpStatus } from "../../constants";
 import { errorHandler, navbarLinks } from "../../services";
+import { Typography } from "antd";
 
 const loginFormFields = [
     {
@@ -36,6 +37,8 @@ const loginFormFields = [
         },
     },
 ];
+
+const { Title } = Typography;
 
 const Login: React.FC = () => {
     const form = useForm({ schema: loginSchema });
@@ -74,10 +77,15 @@ const Login: React.FC = () => {
 
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <WalletOutlined
+                style={{ fontSize: 60 }}
+                className="text-indigo-600"
+            />
+
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                <Title level={4} style={{ textAlign: "center" }}>
                     Sign in to your account
-                </h2>
+                </Title>
             </div>
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <Form ref={form.ref} onSubmit={handleLogin}>
